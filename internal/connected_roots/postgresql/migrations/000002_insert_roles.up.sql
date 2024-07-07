e3f3cbc712b470e1cec6e2633589c9e06a058ba0
Juan Daniel Díaz Matamoros <johnd91@gmail.com>
2024-07-07 13:22:05 +0200

    feat(db): add roles & dummy users

---
+++ internal/connected_roots/postgresql/migrations/000002_insert_roles.up.sql
@@ -0,0 +1,7 @@
+insert into sc_connected_roots.roles (id, name, description, protected, created_at, updated_at, deleted_at)
+values ('01J1G1BE7D2KNDKMHJE9W9TGN1', 'Administrator', 'Admin Role', true, '2024-06-28 20:22:53.537000 +00:00',
+        '2024-06-28 20:22:55.482000 +00:00', null),
+       ('01J1WQQ7RASGG8M6ASYPGAW0RX', 'User', 'User Role', true, '2024-06-28 20:22:53.537000 +00:00',
+        '2024-06-28 20:22:53.537000 +00:00', null),
+       ('01J1WQQZM01ZVWPQWDZ0AAMYB6', 'Technical', 'Technical Role', true, '2024-06-28 20:22:53.537000 +00:00',
+        '2024-06-28 20:22:53.537000 +00:00', null);
---
+++ internal/connected_roots/postgresql/migrations/000003_insert_users.up.sql
@@ -0,0 +1,11 @@
+insert into sc_connected_roots.users (id, name, surname, email, password, telephone, language, role_id, created_at,
+                                      updated_at, deleted_at)
+values ('01J13ASEE53P4GE3370EHXFMTZ', 'Admin', 'User', 'admin@connected-roots.com',
+        '$2a$10$EYZDi/po7FZN/i/jOfG/refW1t3k7NoEpS1rbGvqVikMvmhkPHAoG', '647291885', 'es', '01J1G1BE7D2KNDKMHJE9W9TGN1',
+        '2024-06-23 21:56:53.939000 +00:00', '2024-07-07 10:33:48.178488 +00:00', null),
+       ('01J26CKYMNAFAT0X2HQ4EKWMH0', 'User', 'Dummy', 'user-dummy@dummy-user.com',
+        '$2a$10$EYZDi/po7FZN/i/jOfG/refW1t3k7NoEpS1rbGvqVikMvmhkPHAoG', '647291887', 'es', '01J1WQQ7RASGG8M6ASYPGAW0RX',
+        '2024-07-07 10:42:45.525368 +00:00', '2024-07-07 10:55:15.516250 +00:00', null),
+       ('01J26EPQVX6G99E37DSVJBVMKD', 'Technical', 'User', 'technical@connected-roots.com',
+        '$2a$10$EYZDi/po7FZN/i/jOfG/refW1t3k7NoEpS1rbGvqVikMvmhkPHAoG', '647291886', 'es', '01J1WQQZM01ZVWPQWDZ0AAMYB6',
+        '2024-07-07 10:42:45.525368 +00:00', '2024-07-07 10:55:15.516250 +00:00', null);
